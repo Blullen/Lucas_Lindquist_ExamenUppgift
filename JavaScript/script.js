c28c13b430da8740b1ef5ad8a27b2eb3c108a6d4
Blullen <lucas.lindqquist@gmail.com>
2026-05-22 14:12:21 +0200

    EventListener ("keydown", event(){})

---
+++ JavaScript/script.js
@@ -0,0 +1,9 @@
+console.log("Funkar")
+
+document.addEventListener ("keydown", (event) =>{
+    console.log(event.key);
+
+    if (event.key == "b") {
+        document.getElementById("Bond").style = "color:pink";
+    }
+});
