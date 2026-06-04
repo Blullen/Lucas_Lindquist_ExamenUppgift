1177848cf3c64f46ae7f11dd202ce4289e458e07
Blullen <lucas.lindqquist@gmail.com>
2026-06-04 10:32:14 +0200

    Fullt fungerande tangentbord

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,12 +1,29 @@
+let isCtrlPressed = false;
+
 document.addEventListener("keydown", (event) => {
 
     console.log(event.code);
 
 if(event.code){
-    document.getElementById(event.code).style = "background-color:pink";
     if(event.code == "Enter"){
         document.getElementById("enterBottom").style = "background-color:pink";
     }
+    if (event.code === 'ControlLeft') {
+        isCtrlPressed = true;
+
+        setTimeout(() => {
+            if (isCtrlPressed) {
+                document.getElementById("ControlLeft").style = "background-color:pink";
+            }
+        }, 0);
+        return;
+    }
+    if (event.code === 'AltRight' || event.key === 'AltGraph') {
+        isCtrlPressed = false;
+        document.getElementById("AltRight").style = "background-color:pink";
+        return;
+    }
+    document.getElementById(event.code).style = "background-color:pink";
 }
 });
 document.addEventListener("keyup", (event) => {
@@ -14,9 +31,12 @@ document.addEventListener("keyup", (event) => {
     console.log(event.key);
 
 if(event.code){
-    document.getElementById(event.code).style = "background-color: rgb(85, 40, 30)";
     if(event.code == "Enter"){
         document.getElementById("enterBottom").style = "background-color: rgb(85, 40, 30)";
     }
+    if (event.code === 'ControlLeft') {
+        isCtrlPressed = false;
+    }
+    document.getElementById(event.code).style = "background-color: rgb(85, 40, 30)";
 }
 });
