f551149ee171379ce187afd15fdbd5faa9709d75
Blullen <lucas.lindqquist@gmail.com>
2026-06-04 09:46:11 +0200

    Nu funkar alla knappar som det ska förutom alt gr

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,16 +1,22 @@
 document.addEventListener("keydown", (event) => {
 
-    console.log(event.key);
+    console.log(event.code);
 
-if(event.key){
-    document.getElementById(event.key).style = "background-color:pink";
+if(event.code){
+    document.getElementById(event.code).style = "background-color:pink";
+    if(event.code == "Enter"){
+        document.getElementById("enterBottom").style = "background-color:pink";
+    }
 }
 });
 document.addEventListener("keyup", (event) => {
 
     console.log(event.key);
 
-if(event.key){
-    document.getElementById(event.key).style = "background-color: rgb(85, 40, 30)";
+if(event.code){
+    document.getElementById(event.code).style = "background-color: rgb(85, 40, 30)";
+    if(event.code == "Enter"){
+        document.getElementById("enterBottom").style = "background-color: rgb(85, 40, 30)";
+    }
 }
 });
